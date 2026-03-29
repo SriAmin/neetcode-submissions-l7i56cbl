14e7dd315451541d7891e5f32277447686cc82a3
Sri Amin <sri.amin@gmail.com>
2026-03-29 16:57:02 -0400

    Add: is-anagram - submission-4

---
+++ Data Structures & Algorithms/is-anagram/submission-4.py
@@ -0,0 +1,15 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+
+        str1 = {}
+        str2 = {}
+
+        for i in s:
+            str1[i] = str1[i] + 1 if i in str1 else 1
+        for j in t:
+            str2[j] = str2[j] + 1 if j in str2 else 1
+
+        return str1 == str2
+ 
