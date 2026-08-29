b8b1b763a8100f5296155ca985bf2417c6897054
Sri Amin <sri.amin@gmail.com>
2026-08-28 20:33:24 -0400

    Add: valid-parenthesis-string - submission-1

---
+++ Data Structures & Algorithms/valid-parenthesis-string/submission-1.py
@@ -0,0 +1,22 @@
+class Solution:
+    def checkValidString(self, s: str) -> bool:
+        leftStack, starStack = [], []
+
+        for i in range(len(s)):
+            if s[i] == "(":
+                leftStack.append(i)
+            if s[i] == "*":
+                starStack.append(i)
+            if s[i] == ")":
+                if len(leftStack) >= 1:
+                    leftStack.pop()
+                elif len(starStack) >= 1:
+                    starStack.pop()
+                else:
+                    return False
+        while len(leftStack) >= 1 and len(starStack) >= 1:
+            iL = leftStack.pop()
+            iS = starStack.pop()
+            if iL > iS:
+                return False
+        return len(leftStack) == 0
